482855eefeef784b1a3d196bb938d785a9053285
Pavel Volchenko <2793553@gmail.com>
2023-02-03 21:24:42 +0300

    hw05-task01 complete

--- src/hw05/task01.java
+++ src/hw05/task01.java
@@ -1,24 +1,29 @@
 package hw05;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.HashMap;
 
 public class task01 {
     /*  Реализуйте структуру телефонной книги с помощью HashMap,
         учитывая, что 1 человек может иметь несколько телефонов.    */
     public static void main(String[] args) {
-        System.out.println("""
-                ===== ===== ТЕЛЕФОННАЯ КНИГА ===== =====
-                1. Создать новую запись
-                2. Добавить телефон к существующей записи
-                3. Вывести на экран все записи
-                4. Завершить работу""");
+        System.out.println(showMenu);
+
         db.put("Иван Иванов", List.of("+7(909)909-93-12", "+7(928)912-94-52"));
         db.put("Петя Петров", List.of("+7(999)909-33-22", "+7(906)111-22-52", "+7(159)909-33-55"));
         db.put("Зоя Витальевна", List.of("+7(159)909-33-55", "+7(936)441-22-52",
                 "+7(159)909-33-55", "+7(936)441-22-52"));
 
+        List<String> ph = new ArrayList<>();
+        ph.add("+7(909)909-93-12");
+        ph.add("+7(222)111-44-12");
+        ph.add("+7(333)111-44-12");
+        db.put("Денис Иванов", ph);
+
+        iScanner.useDelimiter("\n");
         while (true) {
             System.out.print("Действие -> ");
             int action = iScanner.nextInt();
@@ -28,35 +33,53 @@ public class task01 {
                 case 3 -> printTelephoneBook();
             }
             if (action == 4) {
-                System.out.println("===== ====== ВСЕГО ДОБРОГО ====== =====");
+                System.out.println("======== ВСЕГО ДОБРОГО ========");
                 break;
             }
         }
     }
 
+    public static String showMenu = """
+            \n======= ТЕЛЕФОННАЯ КНИГА =======
+            1. Создать новую запись
+            2. Добавить телефон к записи
+            3. Вывести на экран все записи
+            4. Завершить работу""";
     public static Scanner iScanner = new Scanner(System.in);
     public static HashMap<String, List> db = new HashMap<>();
 
     public static void createRecord() {
-        iScanner.useDelimiter("\n");
         System.out.print("Имя, фамилия: ");
         String name = iScanner.next();
-        System.out.print("Номера телефонов через пробел: ");
+        System.out.print("Введите номера телефонов через пробел: ");
         db.put(name, List.of(iScanner.next().split(" ")));
     }
 
     public static void addTelephone() {
-        String a = db.get("Петя Петров").toString().replace("[","").replace("]","");
-        a += "+7(900)111-22-33";
-        System.out.println(db.put("Петя Петров", List.of(a.split(", "))));
-
+        TreeMap<Integer, String> abonent = new TreeMap<>();
+        int index = 1;
+        System.out.println("\n======= СПИСОК АБОНЕНТОВ =======");
+        for (var item : db.entrySet()) {
+            abonent.put(index, item.getKey());
+            System.out.println(index + ". " + item.getKey());
+            index++;
+        }
+        System.out.print("Порядковый номер абонента -> ");
+        int id = Integer.parseInt(iScanner.next());
+        List<String> phones = new ArrayList<String>(db.get(abonent.get(id)));
+        System.out.print("Введите номера телефонов через пробел: ");
+        phones.addAll(List.of(iScanner.next().split(" ")));
+        db.put(abonent.get(id), phones);
     }
 
     public static void printTelephoneBook() {
+        System.out.println(showMenu);
+        System.out.println("\n======= СПИСОК АБОНЕНТОВ =======");
         int maxLenght = 0;
         for (var item : db.keySet()) {
             if (item.length() > maxLenght) {
-                maxLenght = item.length() + 2;
+                maxLenght = item.length();
+                maxLenght++;
             }
         }
         for (var item : db.entrySet()) {
--- src/hw05/task02.java
+++ src/hw05/task02.java
@@ -1,5 +1,7 @@
 package hw05;
 
+import java.util.*;
+
 public class task02 {
     /*  Пусть дан список сотрудников:
         Иван Иванов
@@ -22,4 +24,56 @@ public class task02 {
         Иван Ежов
         Написать программу, которая найдет и выведет повторяющиеся имена
         с количеством повторений. Отсортировать по убыванию популярности Имени.     */
+    public static void main(String[] args) {
+        List<String> employees = new ArrayList<>();
+        String data = """
+                Иван Иванов
+                Светлана Петрова
+                Кристина Белова
+                Анна Мусина
+                Анна Крутова
+                Иван Юрин
+                Петр Лыков
+                Иван Иванов
+                Павел Чернов
+                Петр Чернышов
+                Мария Федорова
+                Марина Светлова
+                Мария Савина
+                Иван Иванов
+                Анна Мусина
+                Мария Рыкова
+                Марина Лугова
+                Анна Владимирова
+                Иван Мечников
+                Анна Мусина
+                Петр Петин
+                Иван Ежов""";
+//        Collections.addAll(employees, data.split("\n"));
+//        TreeMap<String, Integer> count = new TreeMap<>();
+//        for (String employee : employees) {
+//            if (!count.containsKey(employee.split(" ")[0])) {
+//                count.put(employee.split(" ")[0], 1);
+//            } else {
+//                count.put(employee.split(" ")[0], count.get(employee.split(" ")[0]) + 1);
+//            }
+//        }
+//        System.out.println(count.entrySet());
+        Collections.addAll(employees, data.split("\n"));
+        TreeMap<Integer, List> count = new TreeMap<>();
+        List<String> name = new ArrayList<>();
+        count.put(1, name);
+        for (String worker: employees) {
+            if (!name.contains(worker.split(" ")[0])) {
+                name.add(worker.split(" ")[0]);
+            } else {
+                name = new ArrayList<>();
+                name.add(worker.split(" ")[0]);
+                count.put(count.lastKey()+1, name);
+            }
+        System.out.println(count);
+        }
+
+    }
 }
+
