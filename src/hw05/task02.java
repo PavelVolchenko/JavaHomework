9eeb52622b8ba854180b24b907f3d59985ad7eb6
Pavel Volchenko <2793553@gmail.com>
2023-02-03 23:20:38 +0300

    hw05 task02 complete

--- src/hw05/task02.java
+++ src/hw05/task02.java
@@ -4,76 +4,72 @@ import java.util.*;
 
 public class task02 {
     /*  Пусть дан список сотрудников:
-        Иван Иванов
-        Светлана Петрова
-        Кристина Белова
-        Анна Мусина
-        Анна Крутова
-        Иван Юрин
-        Петр Лыков
-        Павел Чернов
-        Петр Чернышов
-        Мария Федорова
-        Марина Светлова
-        Мария Савина
-        Мария Рыкова
-        Марина Лугова
-        Анна Владимирова
-        Иван Мечников
-        Петр Петин
-        Иван Ежов
         Написать программу, которая найдет и выведет повторяющиеся имена
         с количеством повторений. Отсортировать по убыванию популярности Имени.     */
-    public static void main(String[] args) {
-        List<String> employees = new ArrayList<>();
-        String data = """
-                Иван Иванов
-                Светлана Петрова
-                Кристина Белова
-                Анна Мусина
-                Анна Крутова
-                Иван Юрин
-                Петр Лыков
-                Иван Иванов
-                Павел Чернов
-                Петр Чернышов
-                Мария Федорова
-                Марина Светлова
-                Мария Савина
-                Иван Иванов
-                Анна Мусина
-                Мария Рыкова
-                Марина Лугова
-                Анна Владимирова
-                Иван Мечников
-                Анна Мусина
-                Петр Петин
-                Иван Ежов""";
-//        Collections.addAll(employees, data.split("\n"));
-//        TreeMap<String, Integer> count = new TreeMap<>();
-//        for (String employee : employees) {
-//            if (!count.containsKey(employee.split(" ")[0])) {
-//                count.put(employee.split(" ")[0], 1);
-//            } else {
-//                count.put(employee.split(" ")[0], count.get(employee.split(" ")[0]) + 1);
-//            }
-//        }
-//        System.out.println(count.entrySet());
-        Collections.addAll(employees, data.split("\n"));
-        TreeMap<Integer, List> count = new TreeMap<>();
-        List<String> name = new ArrayList<>();
-        count.put(1, name);
-        for (String worker: employees) {
-            if (!name.contains(worker.split(" ")[0])) {
-                name.add(worker.split(" ")[0]);
-            } else {
-                name = new ArrayList<>();
-                name.add(worker.split(" ")[0]);
-                count.put(count.lastKey()+1, name);
-            }
-        System.out.println(count);
-        }
+    public static String data = """
+            Иван Иванов
+            Светлана Петрова
+            Кристина Белова
+            Анна Мусина
+            Анна Крутова
+            Иван Юрин
+            Петр Лыков
+            Иван Иванов
+            Павел Чернов
+            Петр Чернышов
+            Мария Федорова
+            Марина Светлова
+            Мария Савина
+            Иван Иванов
+            Анна Мусина
+            Мария Рыкова
+            Марина Лугова
+            Анна Владимирова
+            Иван Мечников
+            Анна Мусина
+            Петр Петин
+            Кристина Белова
+            Иван Ежов""";
 
+    public static void main(String[] args) {
+        List<String> employees = new ArrayList<>(List.of(data.split("\n")));
+        HashMap<String, Integer> countName = new HashMap<>();
+        TreeMap<Integer, List> sortCountName = new TreeMap<>();
+        for (String name : employees) {
+            if (!countName.containsKey(name.split(" ")[0])) {
+                countName.put(name.split(" ")[0], 1);
+            } else {
+                countName.put(name.split(" ")[0], countName.get(name.split(" ")[0]) + 1);
+            }
+        }
+        for (var item : countName.entrySet()) {
+            if (item.getValue() > 1) {
+                if (!sortCountName.containsKey(item.getValue())) {
+                    sortCountName.put(item.getValue(), List.of(item.getKey()));
+                } else {
+                    List<String> temp = new ArrayList<String>(sortCountName.get(item.getValue()));
+                    temp.add(item.getKey());
+                    sortCountName.put(item.getValue(), temp);
+                }
+            }
+        }
+        int maxLength = 0;
+        for (var item :
+                sortCountName.values()) {
+            for (var name : item
+            ) {
+                if (name.toString().length() > maxLength) {
+                    maxLength = name.toString().length();
+                }
+            }
+        }
+        System.out.println("=== Популярные имена ===");
+        for (var item : sortCountName.descendingMap().entrySet()) {
+            for (var name : item.getValue()) {
+                System.out.print(" ".repeat(maxLength - name.toString().length()) + name + " ");
+                System.out.println(item.getKey());
+            }
+        }
     }
 }
 
